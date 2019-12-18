7496898aec795713efba228cdd8f03d467ba6057
Andika Andriana <andriana.mia1@gmail.com>
2019-12-18 14:25:00 +0700

    Tugas 4 React JS
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import MenuMakanan from './Page/MenuMakanan.js'
+
+class App extends Component{
+render() {
+  return(
+    <div>
+      <center>
+        <MenuMakanan/>
+      </center>
+    </div>
+    )
+  }
+}
+
+export default App;
